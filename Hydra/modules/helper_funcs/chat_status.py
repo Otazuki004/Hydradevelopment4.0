4da1828754424696e7bc3eab6fe705abc0810667
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2022-11-16 11:39:32 +0530

    Update chat_status.py

--- Hydra/modules/helper_funcs/chat_status.py
+++ Hydra/modules/helper_funcs/chat_status.py
@@ -442,7 +442,7 @@ def connection_status(func):
     return connected_status
 
 
-from NekoRobot.modules import connection
+from Hydra.modules import connection
 
 connected = connection.connected
 
