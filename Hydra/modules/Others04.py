7fdd29adaf953aff8d587c722fd74e22171378af
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2022-11-16 09:53:24 +0530

    Create Others04.py

---
+++ Hydra/modules/Others04.py
@@ -0,0 +1,23 @@
+from Hydra.events import register
+
+
+@register(pattern=("Bye"))
+async def awake(event):
+    await event.reply("Bye Bye."
+
+
+@register(pattern=("bye"))
+async def awake(event):
+    await event.reply("Bye Bye."
+
+
+@register(pattern=("Bye Boy"))
+async def awake(event):
+    await event.reply("Bye Bye."
+
+
+@register(pattern=("Bye boi"))
+async def awake(event):
+    await event.reply("Bye Bye."
+
+#toon_lisence
