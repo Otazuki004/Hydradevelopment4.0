4a5989db121e32c7f43b4c22fa8ae8bf192fd87c
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-01-12 12:44:05 +0530

    Create Thunivu.py

---
+++ Hydra/modules/Thunivu.py
@@ -0,0 +1,64 @@
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+ImG = "https://te.legra.ph/file/969a3645bb3a6687322e9.jpg"
+
+
+@register(pattern=("Thunivu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, ImG)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Movie = "https://t.me/CSFH005/11"
+
+
+@register(pattern=("Thunivu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Movie)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Moviek = "https://t.me/CSFH005/12"
+
+
+@register(pattern=("Thunivu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Moviek)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Moviey = "https://t.me/CSFH005/13"
+
+
+@register(pattern=("Thunivu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Moviey)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Movieo = "https://t.me/CSFH005/14"
+
+
+@register(pattern=("Thunivu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Movieo)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Moviee = "https://t.me/CSFH005/15"
+
+
+@register(pattern=("Thunivu"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Moviee)
