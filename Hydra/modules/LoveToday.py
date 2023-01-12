081daf73e8f4194aa2d66836367f05464288dc32
OTAZUKIII <115762083+Otazuki004@users.noreply.github.com>
2023-01-12 13:22:26 +0530

    Create LoveToday.py

---
+++ Hydra/modules/LoveToday.py
@@ -0,0 +1,64 @@
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+ImG = "https://te.legra.ph/file/c6ff64c677c2f658431c1.jpg"
+
+
+@register(pattern=("Love Today!"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, ImG)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Movie = "https://t.me/CSFH005/17"
+
+
+@register(pattern=("Love Today!"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Movie)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Moviek = "https://t.me/CSFH005/18"
+
+
+@register(pattern=("Love Today!"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Moviek)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Moviey = "https://t.me/CSFH005/19"
+
+
+@register(pattern=("Love Today!"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Moviey)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Movieo = "https://t.me/CSFH005/20"
+
+
+@register(pattern=("Love Today!"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Movieo)
+
+
+from Hydra import tbot as tbot
+from Hydra.events import register
+
+Moviee = "https://t.me/CSFH005/21"
+
+
+@register(pattern=("Love Today!"))
+async def awake(event):
+    await tbot.send_file(event.chat_id, Moviee)
